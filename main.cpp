60ae3981c8470c2e2aa4fe53399ed09acee762ce
Marcin Majkowski <pl.marcin.m@gmail.com>
2015-05-28 12:33:27 +0200

    Set main window title

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("Prototyping Toolkit for Image Processing");
     w.show();
 
     return a.exec();
