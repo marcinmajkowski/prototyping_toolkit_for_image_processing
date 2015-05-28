05c8562283cdf84bc7256610f8698b8c325ffd87
Marcin Majkowski <pl.marcin.m@gmail.com>
2015-05-28 14:07:47 +0200

    Application centered on the screen at execution

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QStyle>
+#include <QDesktopWidget>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("Prototyping Toolkit for Image Processing");
+
+    // Center a Window on the Screen
+    w.setGeometry(QStyle::alignedRect(
+                      Qt::LeftToRight, Qt::AlignCenter, w.size(),
+                      qApp->desktop()->availableGeometry()));
+
     w.show();
 
     return a.exec();
