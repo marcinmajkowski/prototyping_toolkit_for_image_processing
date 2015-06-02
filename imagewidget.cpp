852f111dc2632d7deece9469c6f13cac08bbd4a8
Marcin Majkowski <pl.marcin.m@gmail.com>
2015-06-02 17:41:51 +0200

    Improved ImageWidget interface (not implemented yet)

--- imagewidget.cpp
+++ imagewidget.cpp
@@ -1,10 +1,16 @@
 #include <QtWidgets>
 
+#include <opencv2/core/core.hpp>
+
 #include "imagewidget.h"
 
 ImageWidget::ImageWidget(QWidget *parent) :
     QScrollArea(parent)
 {
+    // defaults:
+    m_scaleFactor = 1.0;
+    m_fitToWindow = false;
+
     imageLabel = new QLabel;
     imageLabel->setBackgroundRole(QPalette::Base);
     imageLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
@@ -14,37 +20,65 @@ ImageWidget::ImageWidget(QWidget *parent) :
     setWidget(imageLabel);
 }
 
-bool ImageWidget::loadFile(const QString &filename)
+bool ImageWidget::loadFile(const QString &fileName)
+{
+    QImage image(fileName);
+    if (image.isNull()) {
+        QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
+                                 tr("Cannot load %1").arg(QDir::toNativeSeparators(fileName)));
+        setWindowFilePath(QString());
+        imageLabel->setPixmap(QPixmap());
+        imageLabel->adjustSize();
+        return false;
+    }
+    imageLabel->setPixmap(QPixmap::fromImage(image));
+    m_scaleFactor = 1.0;
+
+    if (!m_fitToWindow) {
+        imageLabel->adjustSize();
+    }
+
+    setWindowFilePath(fileName);
+    return true;
+}
+
+void ImageWidget::setFitToWindow(bool value)
+{
+    m_fitToWindow = value;
+
+    //TODO insert here what was in fitToWindow function
+}
+
+bool ImageWidget::fitToWindow() const
+{
+    return m_fitToWindow;
+}
+
+void ImageWidget::setScaleFactor(double value)
+{
+    m_scaleFactor = value;
+
+    //TODO insert here what was in zoomIn and zoomOut
+    //TODO and in scalelImage()
+}
+
+double ImageWidget::scaleFactor() const
+{
+    return m_scaleFactor;
+}
+
+void ImageWidget::setPixmap(const QPixmap &)
 {
 
 }
 
-void ImageWidget::zoomIn()
+void ImageWidget::setMat(const cv::Mat)
 {
 
 }
 
-void ImageWidget::zoomOut()
-{
-
-}
-
-void ImageWidget::normalSize()
-{
-
-}
-
-void ImageWidget::fitToWindow()
-{
-
-}
-
-void ImageWidget::scaleImage(double factor)
-{
-
-}
 
 void ImageWidget::adjustScrollBar(QScrollBar *scrollBar, double factor)
 {
-
+    //TODO
 }
--- imagewidget.h
+++ imagewidget.h
@@ -2,6 +2,7 @@
 #define IMAGEWIDGET_H
 
 #include <QScrollArea>
+#include <opencv2/core/core.hpp>
 
 class QLabel;
 class QString;
@@ -14,22 +15,26 @@ public:
 
 /* loadFile() is a member of ImageWidget class since this widget
  * knows the best what files it can handle. */
-    bool loadFile(const QString &filename);
+    //TODO it was stupid since it can handle QImage/QPixmap
+    //TODO move it out from here
+    bool loadFile(const QString &fileName);
 
-signals:
+    void setFitToWindow(bool fitToWindow);
+    bool fitToWindow() const;
+
+    void setScaleFactor(double scaleFactor);
+    double scaleFactor() const;
 
 public slots:
-    void zoomIn();
-    void zoomOut();
-    void normalSize();
-    void fitToWindow();
+    void setPixmap(const QPixmap &);
+    void setMat(const cv::Mat);
 
 private:
-    void scaleImage(double factor);
     void adjustScrollBar(QScrollBar *scrollBar, double factor);
 
     QLabel *imageLabel;
-    double scaleFactor;
+    double m_scaleFactor;
+    bool m_fitToWindow;
 };
 
 #endif // IMAGEWIDGET_H
