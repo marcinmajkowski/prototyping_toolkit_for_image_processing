8f0b23a1b64c51faa581b703be4290e0a3c08e7f
Marcin Majkowski <pl.marcin.m@gmail.com>
2015-06-15 16:35:50 +0200

    Added setData() implementation to PipelineModel

--- pipelinemodel.cpp
+++ pipelinemodel.cpp
@@ -6,11 +6,13 @@ PipelineModel::PipelineModel(QObject *parent) :
     QAbstractListModel(parent)
 {
     //TODO below only testing
-    m_filters.push_back(QSharedPointer<Filter>(new AdaptiveThresholdFilter));
-    m_filters.push_back(QSharedPointer<Filter>(new AdaptiveThresholdFilter));
-    m_filters.push_back(QSharedPointer<Filter>(new AdaptiveThresholdFilter));
+//    m_filters.push_back(QSharedPointer<Filter>(new AdaptiveThresholdFilter));
+//    m_filters.push_back(QSharedPointer<Filter>(new AdaptiveThresholdFilter));
+//    m_filters.push_back(QSharedPointer<Filter>(new AdaptiveThresholdFilter));
 //    m_filters.insert(m_filters.size(), 3, QSharedPointer<Filter>());
 //    insertRow(rowCount());
+//    setData(index(1), QVariant("Adaptive threshold"));
+//    setData(index(2), QVariant("Other filter"));
 }
 
 int PipelineModel::rowCount(const QModelIndex &/*parent*/) const
@@ -50,5 +52,30 @@ bool PipelineModel::insertRows(int row, int count, const QModelIndex &parent)
     beginInsertRows(parent, row, row + count - 1);
     m_filters.insert(row, count, QSharedPointer<Filter>());
     endInsertRows();
+    return true;
+}
+
+bool PipelineModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    if (role != Qt::EditRole) {
+        return false;
+    }
+
+    int row = index.row();
+
+    //TODO is it necessary here?
+    if (row > m_filters.size()) {
+        return false;
+    }
+
+    QString filterName = value.toString();
+
+    //TODO use factory here
+    if (filterName == "Adaptive threshold") {
+        m_filters[row] = QSharedPointer<Filter>(new AdaptiveThresholdFilter);
+    } else {
+        m_filters[row] = QSharedPointer<Filter>();
+    }
+
     return true;
 }
--- pipelinemodel.h
+++ pipelinemodel.h
@@ -16,6 +16,7 @@ public:
     int rowCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
     bool insertRows(int row, int count, const QModelIndex &parent = QModelIndex()) Q_DECL_OVERRIDE;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) Q_DECL_OVERRIDE;
 
 signals:
 
