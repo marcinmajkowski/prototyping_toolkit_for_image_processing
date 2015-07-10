7eab6009df2b27edc39a5efca99f947887dbe75a
Marcin Majkowski <pl.marcin.m@gmail.com>
2015-07-10 23:33:16 +0200

    Add DilateFilter to the factory.

--- filterfactory.cpp
+++ filterfactory.cpp
@@ -5,6 +5,7 @@
 #include "Filters/adaptivethresholdfilter.h"
 #include "Filters/thresholdfilter.h"
 #include "Filters/colorspaceconversionfilter.h"
+#include "Filters/dilatefilter.h"
 
 FilterFactory::FilterFactory(QObject *parent) :
     QObject(parent)
@@ -21,6 +22,8 @@ Filter *FilterFactory::create(const QString &type, FilterObserver *observer, QOb
         result = new ThresholdFilter(observer, parent);
     } else if (type == "Color space conversion") {
         result = new ColorSpaceConversionFilter(observer, parent);
+    } else if (type == "Dilate") {
+        result = new DilateFilter(observer, parent);
     } else {
         qDebug() << "FilterFactory::create(): not known filter type" << type;
     }
