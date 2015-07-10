e8ad47d45ced6a8be7547c309661230e56dd3310
Marcin Majkowski <pl.marcin.m@gmail.com>
2015-07-10 23:43:31 +0200

    Implement ErodeFilter default values and process().

--- Filters/erodefilter.cpp
+++ Filters/erodefilter.cpp
@@ -1,6 +1,23 @@
+#include <QDebug>
+#include <QtWidgets>
+
+#include <opencv2/imgproc/imgproc.hpp>
+
 #include "erodefilter.h"
 
-ErodeFilter::ErodeFilter(QObject *parent) :
-    Filter(parent)
+ErodeFilter::ErodeFilter(FilterObserver *observer, QObject *parent) :
+    Filter("Dilate", observer, parent),
+    m_kernel(cv::Mat()),
+    m_anchor(cv::Point(-1, - 1)),
+    m_iterations(1),
+    m_borderType(cv::BORDER_CONSTANT),
+    m_borderValue(cv::morphologyDefaultBorderValue())
 {
+}
+
+cv::Mat &ErodeFilter::process(cv::Mat &input) const
+{
+    cv::erode(input, input, m_kernel, m_anchor, m_iterations, m_borderType, m_borderValue);
+
+    return input;
 }
--- Filters/erodefilter.h
+++ Filters/erodefilter.h
@@ -1,16 +1,23 @@
 #ifndef ERODEFILTER_H
 #define ERODEFILTER_H
 
+#include "filter.h"
+
 class ErodeFilter : public Filter
 {
     Q_OBJECT
 public:
-    explicit ErodeFilter(QObject *parent = 0);
-
-signals:
+    explicit ErodeFilter(FilterObserver *observer = 0, QObject *parent = 0);
+    cv::Mat &process(cv::Mat &input) const Q_DECL_OVERRIDE;
 
 public slots:
 
+private:
+    cv::Mat m_kernel;
+    cv::Point m_anchor;
+    int m_iterations;
+    int m_borderType;
+    cv::Scalar m_borderValue;
 };
 
 #endif // ERODEFILTER_H
