2a9b5bea41741ff57c430d0898e216d6a24b7641
Marcin Majkowski <pl.marcin.m@gmail.com>
2015-07-10 23:33:59 +0200

    Implement DilateFilter default values and process().

--- Filters/dilatefilter.cpp
+++ Filters/dilatefilter.cpp
@@ -1,6 +1,23 @@
+#include <QDebug>
+#include <QtWidgets>
+
+#include <opencv2/imgproc/imgproc.hpp>
+
 #include "dilatefilter.h"
 
-DilateFilter::DilateFilter(QObject *parent) :
-    Filter(parent)
+DilateFilter::DilateFilter(FilterObserver *observer, QObject *parent) :
+    Filter("Dilate", observer, parent),
+    m_kernel(cv::Mat()),
+    m_anchor(cv::Point(-1, - 1)),
+    m_iterations(1),
+    m_borderType(cv::BORDER_CONSTANT),
+    m_borderValue(cv::morphologyDefaultBorderValue())
 {
+}
+
+cv::Mat &DilateFilter::process(cv::Mat &input) const
+{
+    cv::dilate(input, input, m_kernel, m_anchor, m_iterations, m_borderType, m_borderValue);
+
+    return input;
 }
--- Filters/dilatefilter.h
+++ Filters/dilatefilter.h
@@ -1,16 +1,23 @@
 #ifndef DILATEFILTER_H
 #define DILATEFILTER_H
 
+#include "filter.h"
+
 class DilateFilter : public Filter
 {
     Q_OBJECT
 public:
-    explicit DilateFilter(QObject *parent = 0);
-
-signals:
+    explicit DilateFilter(FilterObserver *observer = 0, QObject *parent = 0);
+    cv::Mat &process(cv::Mat &input) const Q_DECL_OVERRIDE;
 
 public slots:
 
+private:
+    cv::Mat m_kernel;
+    cv::Point m_anchor;
+    int m_iterations;
+    int m_borderType;
+    cv::Scalar m_borderValue;
 };
 
 #endif // DILATEFILTER_H
