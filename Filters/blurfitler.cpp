4ee0d41e4275fa19cc0a8b747a5825c9c6a438a9
Marcin Majkowski <pl.marcin.m@gmail.com>
2015-07-22 23:29:21 +0200

    Add functions declarations in BlurFilter.

--- Filters/blurfitler.cpp
+++ Filters/blurfitler.cpp
@@ -1,5 +1,26 @@
 #include "blurfitler.h"
 
-BlurFitler::BlurFitler()
+BlurFitler::BlurFitler(FilterObserver *observer, QObject *parent)
 {
+
+}
+
+QStringList BlurFitler::codeSnippet() const
+{
+
+}
+
+QDialog *BlurFitler::createDialog(QWidget *parent)
+{
+
+}
+
+cv::Mat &BlurFitler::process(cv::Mat &input)
+{
+
+}
+
+void BlurFitler::setBorderType(const QString &type)
+{
+
 }
--- Filters/blurfitler.h
+++ Filters/blurfitler.h
@@ -1,10 +1,27 @@
 #ifndef BLURFITLER_H
 #define BLURFITLER_H
 
+#include <QMap>
+
+#include "filter.h"
+
 class BlurFitler : public Filter
 {
+    Q_OBJECT
 public:
-    BlurFitler();
+    explicit BlurFitler(FilterObserver *observer = 0, QObject *parent = 0);
+    QStringList codeSnippet() const Q_DECL_OVERRIDE;
+    QDialog *createDialog(QWidget *parent = 0) Q_DECL_OVERRIDE;
+    cv::Mat &process(cv::Mat &input) Q_DECL_OVERRIDE;
+
+public slots:
+    void setBorderType(const QString &type);
+
+private:
+    cv::Size m_size;
+    cv::Point m_anchor;
+    int m_borderType;
+    QMap<int, QString> m_borderTypeMap;
 };
 
 #endif // BLURFITLER_H
