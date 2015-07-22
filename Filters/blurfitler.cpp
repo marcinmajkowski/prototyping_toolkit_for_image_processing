40a0e0e577bce41def16a5c4210ddcb88de8e904
Marcin Majkowski <pl.marcin.m@gmail.com>
2015-07-23 00:12:23 +0200

    Put default implementations in BlurFilter.

--- Filters/blurfitler.cpp
+++ Filters/blurfitler.cpp
@@ -1,26 +1,43 @@
+#include <QDebug>
+#include <QtWidgets>
+
+#include <opencv2/imgproc/imgproc.hpp>
+
 #include "blurfilter.h"
 
-BlurFilter::BlurFilter(FilterObserver *observer, QObject *parent)
+BlurFilter::BlurFilter(FilterObserver *observer, QObject *parent) :
+    Filter("Blur", observer, parent),
+    m_size(cv::Size(5, 5)),
+    m_anchor(cv::Point(-1, -1)),
+    m_borderType(cv::BORDER_DEFAULT)
 {
-
+    m_borderTypeMap.insert(cv::BORDER_CONSTANT, "cv::BORDER_CONSTANT");
+    m_borderTypeMap.insert(cv::BORDER_DEFAULT, "cv::BORDER_DEFAULT");
+    m_borderTypeMap.insert(cv::BORDER_REFLECT, "cv::BORDER_REFLECT");
+    m_borderTypeMap.insert(cv::BORDER_REFLECT_101, "cv::BORDER_REFLECT_101");
+    m_borderTypeMap.insert(cv::BORDER_REPLICATE, "cv::BORDER_REPLICATE");
+    m_borderTypeMap.insert(cv::BORDER_WRAP, "cv::BORDER_WRAP"); // cv::exception
 }
 
 QStringList BlurFilter::codeSnippet() const
 {
-
+    //TODO
+    return Filter::codeSnippet();
 }
 
 QDialog *BlurFilter::createDialog(QWidget *parent)
 {
-
+    //TODO
+    return Filter::createDialog(parent);
 }
 
 cv::Mat &BlurFilter::process(cv::Mat &input)
 {
-
+    //TODO
+    return Filter::process(input);
 }
 
 void BlurFilter::setBorderType(const QString &type)
 {
-
+    //TODO
 }
