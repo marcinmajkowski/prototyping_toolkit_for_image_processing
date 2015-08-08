72d114f9164c645e36285f1e4af73815a41b2933
Marcin Majkowski <pl.marcin.m@gmail.com>
2015-08-09 01:13:57 +0200

    Implement AbsoluteDifferenceFilter using BinaryOperationFilter base class

--- Filters/absolutedifferencefilter.cpp
+++ Filters/absolutedifferencefilter.cpp
@@ -8,144 +8,18 @@
 
 AbsoluteDifferenceFilter::AbsoluteDifferenceFilter(FilterObserver *observer,
                                                    QObject *parent) :
-    Filter(filterName, observer, parent),
-    m_convertSecondInput(false),
-    m_inputsSizesMatch(false),
-    m_adjustSecondInput(NONE)
+    BinaryOperationFilter(filterName, observer, parent)
 {
+}
+
+QString AbsoluteDifferenceFilter::binaryOperationName() const
+{
+    return "absdiff";
+}
+
+void AbsoluteDifferenceFilter::binaryOperation(cv::Mat &input, cv::Mat &secondInput)
+{
+    cv::absdiff(input, secondInput, input);
 }
 
 QString AbsoluteDifferenceFilter::filterName = "Absolute difference";
-
-QStringList AbsoluteDifferenceFilter::codeSnippet() const
-{
-    QString referenceImageName = "reference_image";
-
-    QStringList snippet;
-
-    snippet << QString("%1 = cv::imread(\"%2\", %3);")
-               .arg(referenceImageName)
-               .arg(m_secondInputPath)
-               .arg(m_convertSecondInput
-                    ? "CV_LOAD_IMAGE_GRAYSCALE"
-                    : "CV_LOAD_IMAGE_COLOR");
-
-    snippet << QString("cv::absdiff(%1, %2, %3);")
-               .arg("img")
-               .arg(referenceImageName)
-               .arg("img");
-
-    return snippet;
-}
-
-cv::Mat &AbsoluteDifferenceFilter::process(cv::Mat &input)
-{
-    if (input.size != m_secondInputImage.size) {
-        m_inputsSizesMatch = false;
-        return input;
-    }
-
-    cv::Mat secondInput;
-
-    switch(input.type()) {
-    case CV_8UC1:
-        cv::cvtColor(m_secondInputImage, secondInput, CV_RGB2GRAY);
-        m_convertSecondInput = true;
-        break;
-    case CV_8UC3:
-        secondInput = m_secondInputImage;
-        m_convertSecondInput = false;
-        break;
-    case CV_8UC4:
-        cv::cvtColor(m_secondInputImage, secondInput, CV_RGB2RGBA);
-        m_convertSecondInput = false;
-        break;
-    }
-
-    cv::absdiff(input, secondInput, input);
-
-    m_inputsSizesMatch = true;
-
-    return input;
-}
-
-void AbsoluteDifferenceFilter::read(QDataStream &data)
-{
-    data >> m_secondInputPath;
-    m_secondInputImage = cv::imread(m_secondInputPath.toUtf8().constData(), CV_LOAD_IMAGE_COLOR);
-    if (m_secondInputImage.empty()) {
-        qDebug() << "File" << m_secondInputPath << "does not exist.";
-    }
-
-    emit updated();
-}
-
-void AbsoluteDifferenceFilter::write(QDataStream &data) const
-{
-    data << m_secondInputPath;
-}
-
-void AbsoluteDifferenceFilter::browse()
-{
-    //TODO change with cv::imread supported types
-    QStringList mimeTypeFilters;
-    foreach (const QByteArray &mimeTypeName,
-             QImageReader::supportedMimeTypes()) {
-        mimeTypeFilters.append(mimeTypeName);
-    }
-    mimeTypeFilters.sort();
-    const QStringList picturesLocations =
-            QStandardPaths::standardLocations(QStandardPaths::PicturesLocation);
-    QFileDialog dialog(m_pathLineEdit, tr("Open File"), picturesLocations.isEmpty() ?
-                           QDir::currentPath() : picturesLocations.first());
-    dialog.setAcceptMode(QFileDialog::AcceptOpen);
-    dialog.setMimeTypeFilters(mimeTypeFilters);
-    dialog.selectMimeTypeFilter("image/jpeg");
-
-    while (dialog.exec() == QDialog::Accepted &&
-           !loadSecondInput(dialog.selectedFiles().first())) {
-    }
-
-    emit updated();
-}
-
-QLayout *AbsoluteDifferenceFilter::dialogParametersGroupLayout()
-{
-    m_pathLineEdit = new QLineEdit(m_secondInputPath);
-    QPushButton *button = new QPushButton("Browse");
-    connect(button, SIGNAL(clicked()), this, SLOT(browse()));
-
-    QHBoxLayout *browseLayout = new QHBoxLayout;
-    browseLayout->addWidget(m_pathLineEdit);
-    browseLayout->addWidget(button);
-
-    QFormLayout *formLayout = new QFormLayout;
-    formLayout->addRow(new QLabel("second input path:"), browseLayout);
-
-    return formLayout;
-}
-
-QLabel *AbsoluteDifferenceFilter::dialogDescriptionLabel()
-{
-    return Filter::dialogDescriptionLabel();
-}
-
-bool AbsoluteDifferenceFilter::loadSecondInput(QString fileName)
-{
-    m_secondInputImage = cv::imread(fileName.toUtf8().constData(),
-                                    CV_LOAD_IMAGE_COLOR);
-    if (m_secondInputImage.empty()) {
-        QMessageBox::information(m_pathLineEdit,
-                                 QGuiApplication::applicationDisplayName(),
-                                 tr("Cannot load %1")
-                                 .arg(QDir::toNativeSeparators(fileName)));
-        return false;
-    }
-
-    m_secondInputPath = fileName;
-    if (m_pathLineEdit) {
-        m_pathLineEdit->setText(m_secondInputPath);
-    }
-
-    return true;
-}
--- Filters/absolutedifferencefilter.h
+++ Filters/absolutedifferencefilter.h
@@ -3,48 +3,20 @@
 
 #include <opencv2/core/core.hpp>
 
-#include "filter.h"
+#include "binaryoperationfilter.h"
 
-class QLineEdit;
-
-class AbsoluteDifferenceFilter : public Filter
+class AbsoluteDifferenceFilter : public BinaryOperationFilter
 {
     Q_OBJECT
 public:
     Q_INVOKABLE explicit AbsoluteDifferenceFilter(FilterObserver *observer = 0,
                                                   QObject *parent = 0);
-    QStringList codeSnippet() const Q_DECL_OVERRIDE;
-    cv::Mat &process(cv::Mat &input) Q_DECL_OVERRIDE;
-
-    void read(QDataStream &data) Q_DECL_OVERRIDE;
-    void write(QDataStream &data) const Q_DECL_OVERRIDE;
 
     static QString filterName;
 
-public slots:
-    void browse();
-
-protected:
-    QLayout *dialogParametersGroupLayout() Q_DECL_OVERRIDE;
-    QLabel *dialogDescriptionLabel() Q_DECL_OVERRIDE;
-    bool loadSecondInput(QString fileName);
-
-private:
-    QString m_secondInputPath;
-    cv::Mat m_secondInputImage;
-
-    enum AdjustSecondInput {
-        NONE,
-        SCALE,
-        CROP
-    };
-
-    bool m_convertSecondInput;
-    bool m_inputsSizesMatch;
-
-    AdjustSecondInput m_adjustSecondInput;
-
-    QLineEdit *m_pathLineEdit;
+    QString binaryOperationName() const;
+    void binaryOperation(cv::Mat &input,
+                         cv::Mat &secondInput);
 };
 
 Q_DECLARE_METATYPE(AbsoluteDifferenceFilter *)
